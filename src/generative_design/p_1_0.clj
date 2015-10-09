6dbe63c5242716ffc11a1e348411e3cb7f8762ff
Nils Blum-Oeste <nils.blum-oeste@lambdawerk.com>
2015-10-09 19:49:58 +0200

    Initial commit with P.1.0 example

---
+++ src/generative_design/p_1_0.clj
@@ -0,0 +1,27 @@
+(ns generative-design.p-1-0
+  (:require [quil
+             [core :as q]
+             [middleware :as m]]))
+
+(defn setup []
+  (q/no-cursor))
+
+(defn update-state [state]
+  state)
+
+(defn draw-state [state]
+  (q/color-mode :hsb 360 100 100)
+  (q/rect-mode :center)
+  (q/no-stroke)
+  (q/background (/ (q/mouse-y) 2) 100 100)
+  (q/fill (- 360 (/ (q/mouse-y) 2)) 100 100)
+  (q/rect 360 360 (inc (q/mouse-x)) (inc (q/mouse-x))))
+
+(q/defsketch hello-color
+  :title "Hello Color P.1.0"
+  :size [720 720]
+  :setup setup
+  :update update-state
+  :draw draw-state
+  :features [:keep-on-top :resizable]
+  :middleware [m/fun-mode])
---
+++ test/generative_design/core_test.clj
@@ -0,0 +1,7 @@
+(ns generative-design.core-test
+  (:require [clojure.test :refer :all]
+            [generative-design.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
